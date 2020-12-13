463e22370ae062d0fd50b8b26192b21c45808eb8
John Micko <jrmicko5012@gmail.com>
2020-12-13 04:39:40 -0600

    Add feeling value to state

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -3,13 +3,30 @@ import React, { Component } from 'react';
 
 // class
 class Feeling extends Component {
+
+    state = {
+        feeling: ''
+    }
+
+    handleChange = (event, inputType) => {
+        console.log(inputType, '=', event.target.value)
+        this.setState({
+            [inputType]: event.target.value,
+        })
+    }
+
     render() {
         return (
             <div>
+                {JSON.stringify(this.state)}
                 <h2>How are you feeling today?</h2>
                 <form>
                     <label>Feeling?</label>
-                    <input type="number" min="1" max="5" />
+                    <input
+                        type="number"
+                        min="1"
+                        max="5"
+                        onChange={(event) => this.handleChange(event, 'feeling')} />
                     <button type="submit">Next</button>
                 </form>
             </div>
