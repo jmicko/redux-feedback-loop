6da708cc7c2262c59c254130616b19fc499e402e
John Micko <jrmicko5012@gmail.com>
2020-12-13 17:50:46 -0600

    create Review map

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,16 +1,27 @@
 // imports
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // class
 class Review extends Component {
     render() {
         return (
             <div>
-                <h2>hello from Review component</h2>
+                <h2>Review Your Feedback</h2>
+                {this.props.reduxState.feedbackReducer.map((feedbackItem, i) => {
+                    return(
+                        // checking to make sure evertthing comes back right from dom
+                        <p key={i}>{JSON.stringify(feedbackItem)}</p>
+                        )
+                })}
             </div>
         ) // end return
     } // end render
 } // end class
 
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+
 // export
-export default Review;
+export default connect(putReduxStateOnProps)(Review);
