2330760f8816fd14a0f9486755a103328fa10ae2
John Micko <jrmicko5012@gmail.com>
2020-12-13 06:43:29 -0600

    connect to redux

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,5 +1,7 @@
 // imports
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 // class
 class Feeling extends Component {
@@ -15,10 +17,16 @@ class Feeling extends Component {
         })
     }
 
+    submit = () => {
+        console.log('saving feeling:', this.state.feeling);
+        this.props.dispatch({ type: 'ADD_RESPONSE', payload: this.state.feeling });
+        // this.props.history.push('/understanding');
+    }
+
     render() {
         return (
             <div>
-                {JSON.stringify(this.state)}
+                {JSON.stringify(this.props)}
                 <h2>How are you feeling today?</h2>
                 <form>
                     <label>Feeling?</label>
@@ -27,12 +35,16 @@ class Feeling extends Component {
                         min="1"
                         max="5"
                         onChange={(event) => this.handleChange(event, 'feeling')} />
-                    <button type="submit">Next</button>
+                    <button type="submit" onClick={this.submit}>Next</button>
                 </form>
             </div>
         ) // end return
     } // end render
 } // end class
 
-// export
-export default Feeling;
+//export
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+
+export default connect(putReduxStateOnProps)(Feeling);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+//feedback reducer
+const feedbackReducer = (state=[], action) => {
+    if (action.type === 'ADD_RESPONSE'){
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    combineReducers({
+        feedbackReducer
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
