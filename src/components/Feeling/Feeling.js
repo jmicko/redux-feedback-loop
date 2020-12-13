30107817e2846c6ff2a6b608c62b81213b0f0f20
John Micko <jrmicko5012@gmail.com>
2020-12-13 04:33:03 -0600

    create form

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -6,7 +6,12 @@ class Feeling extends Component {
     render() {
         return (
             <div>
-                <h2>hello from Feeling component</h2>
+                <h2>How are you feeling today?</h2>
+                <form>
+                    <label>Feeling?</label>
+                    <input type="number" min="1" max="5" />
+                    <button type="submit">Next</button>
+                </form>
             </div>
         ) // end return
     } // end render
