45aeb10473a0fd34744a48c2bcefa8958d2be8c5
John Micko <jrmicko5012@gmail.com>
2020-12-14 01:53:57 -0600

    set up node feedback POST route

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
+app.post('/feedback', (req, res) => {
+    console.log('In POST route', req.body);
+    res.sendStatus(201)
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
