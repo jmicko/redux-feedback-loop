f2236529f65bc44911024fbb6191609e7660cb5b
John Micko <jrmicko5012@gmail.com>
2020-12-15 02:41:52 -0600

    remove form tag to fix error

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -6,23 +6,28 @@ import { withRouter } from 'react-router-dom';
 // class
 class Input extends Component {
 
-    // feedbackValue = this.props.feebackValue;
-
     state = {
         feedbackItem: []
     }
 
     submit = () => {
         console.log('saving feedback:', this.state.feedbackItem);
+        // send current state to redux
         this.props.dispatch({ type: 'ADD_RESPONSE', payload: this.state.feedbackItem });
+        // navigate to whichever component is next,
+        // as determinded by App
         this.props.history.push(`/${[this.props.next]}`);
     }
 
-    handleChange = (event, inputType) => {
-        console.log(inputType, '=', event.target.value)
+    handleChange = (event, feedbackName) => {
+        console.log(feedbackName, '=', event.target.value)
         this.setState({
             feedbackItem: {
-                [inputType]: event.target.value,
+                // can take any feedback and make it the state, 
+                // ultimately adding it to the reducer object
+                // feedback can easily grow with just a new component
+                // no need to modify state or reducer
+                [feedbackName]: event.target.value,
             }
         })
     }
@@ -31,23 +36,20 @@ class Input extends Component {
     render() {
         return (
             <div>
-                {/* <p>props:</p>
-                {JSON.stringify(this.props)}
-                <p>state:</p>
-                {JSON.stringify(this.state)} */}
-                <form>
-                    <label>{this.props.feebackName}?</label>
-                    {(this.props.feedbackType === "text")
-                        ? <textarea
-                            onChange={(event) => this.handleChange(event, this.props.feebackName)} />
-                        : <input
-                            type="number"
-                            min="1"
-                            max="5"
-                            onChange={(event) => this.handleChange(event, this.props.feebackName)} />
-                    }
-                    <button type="submit" onClick={this.submit}>Next</button>
-                </form>
+                <label>{this.props.feebackName}?</label>
+                {/* if feedback needs a text input, display it.
+                        otherwise display a number input */}
+                {(this.props.feedbackType === "text")
+                    ? <textarea
+                        onChange={(event) => this.handleChange(event, this.props.feebackName)} />
+                    : <input
+                        type="number"
+                        min="1"
+                        max="5"
+                        onChange={(event) => this.handleChange(event, this.props.feebackName)} />
+                }
+                {/* submit input */}
+                <button type="submit" onClick={this.submit}>Next</button>
             </div>
         ) // end return
     } // end render
@@ -57,5 +59,5 @@ const putReduxStateOnProps = (reduxState) => ({
     reduxState
 })
 
-// export
+// export, need withRouter because component is called from many components, not App
 export default withRouter(connect(putReduxStateOnProps)(Input));
